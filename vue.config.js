80627021e6fdca42e9d41a2cf7784383348a9be3
Semenov Ihor <Semenovwork27@gmail.com>
2023-05-23 11:17:30 +0300

    spa fix deploy

--- vue.config.js
+++ vue.config.js
@@ -9,15 +9,15 @@ module.exports = defineConfig({
 module.exports = {
   publicPath: '/untitled-vue/',
   
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Home Page',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-      },
-    },
+  // pages: {
+  //   index: {
+  //     entry: 'src/main.js',
+  //     template: 'public/index.html',
+  //     filename: 'index.html',
+  //     title: 'Home Page',
+  //     chunks: ['chunk-vendors', 'chunk-common', 'index']
+  //     },
+  //   },
     
   }
 
